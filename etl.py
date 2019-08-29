f06a86fcf054e521842596d5424776016535dfb1
jameskomo <james.komoh@gmail.com>
2019-08-29 13:02:37 +0300

    Data source read and passed to data frame to allow two dimensional handling with indexing

--- etl.py
+++ etl.py
@@ -4,3 +4,4 @@ import pandas as pd
 ##  To read and work on the data
 import sqlite3  ##  To work on the database
 df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  Data retreival from the URL.
+print(df)
