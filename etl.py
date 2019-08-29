5c1622e260e30d990d5b1c021ddaf6bf575fed32
jameskomo <james.komoh@gmail.com>
2019-08-29 13:21:52 +0300

    column labels of the DataFrame defined from Year to the two months each as each value spans a 2-month season

--- etl.py
+++ etl.py
@@ -7,4 +7,7 @@ df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  
 df = df.iloc[:41]  ##  Unncessary text removed from the data
 list = [(df.iloc[row][0].split()) for row in range(len(df))]  ##  Creating a list with list comprehension consisting of lists(yearly data)
 dfob = pd.DataFrame(list)  ##  Converting this list into a DataFrame
-print(dfob)
+
+dfob.columns = ['Year', '12-01', '01-02', '02-03', '03-04', '04-05', '05-06', '06-07', '07-08', '08-09', '09-10', '10-11', '11-12']
+##  The header names are renewed.
+print(dfob.columns)
