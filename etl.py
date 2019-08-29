2da9c4c5818489de13cad3706976a89a0860daa5
jameskomo <james.komoh@gmail.com>
2019-08-29 13:10:45 +0300

    List created using list comprehension consisting of lists to allow showing yearly data

--- etl.py
+++ etl.py
@@ -5,4 +5,5 @@ import pandas as pd
 import sqlite3  ##  To work on the database
 df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  Data retreival from the URL.
 df = df.iloc[:41]  ##  Unncessary text removed from the data
-print(df)
+list = [(df.iloc[row][0].split()) for row in range(len(df))]  ##  Creating a list with list comprehension consisting of lists(yearly data)
+print(list)
