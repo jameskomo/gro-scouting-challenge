23a0815ec4d78f6d9c26fcaa9a73c4d1280bab1e
jameskomo <james.komoh@gmail.com>
2019-08-29 12:51:47 +0300

    Python Pandas library installed in virtual environment from pypi and imported to etl.py

---
+++ etl.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd  
+##  To read and work on the data
+import sqlite3  ##  To work on the database
+df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  Data retreival from the URL.
