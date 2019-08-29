12b339d1744bf8dcd4c6c3ce550108dd5b426a29
jameskomo <james.komoh@gmail.com>
2019-08-29 13:08:18 +0300

    Unnecessary text removed by using iloc method to retrieve rows from read data frame

--- etl.py
+++ etl.py
@@ -4,4 +4,5 @@ import pandas as pd
 ##  To read and work on the data
 import sqlite3  ##  To work on the database
 df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  Data retreival from the URL.
+df = df.iloc[:41]  ##  Unncessary text removed from the data
 print(df)
