9c51fa68063dd60b70ad59e2c67fabc72845c662
jameskomo <james.komoh@gmail.com>
2019-08-29 13:18:49 +0300

    List converted back to data frame to allow manipulation with pandas

--- etl.py
+++ etl.py
@@ -6,4 +6,5 @@ import sqlite3  ##  To work on the database
 df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  Data retreival from the URL.
 df = df.iloc[:41]  ##  Unncessary text removed from the data
 list = [(df.iloc[row][0].split()) for row in range(len(df))]  ##  Creating a list with list comprehension consisting of lists(yearly data)
-print(list)
+dfob = pd.DataFrame(list)  ##  Converting this list into a DataFrame
+print(dfob)
