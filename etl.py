aba1037f6cb67a783e072b1ea9dbe50b8364a71e
jameskomo <james.komoh@gmail.com>
2019-08-31 19:11:13 +0300

    SQLite Installed and DB Browser installed as well for testing purposes. Testing done and writing to DB confirmed as working OK

--- etl.py
+++ etl.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd  
-##  To read and work on the data
-import sqlite3  ##  To work on the database
+import sqlite3 
+
 df = pd.read_csv("https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data")  ##  Data retreival from the URL.
 df = df.iloc[:41]  ##  Unncessary text removed from the data
 list = [(df.iloc[row][0].split()) for row in range(len(df))]  ##  Creating a list with list comprehension consisting of lists(yearly data)
@@ -44,5 +44,5 @@ def add_data(data):
             ##  The 'inserting' query in SQL is run
             con.commit()  
 
-add_data(dfob)  ##  We run the function to add the data to the table   
-con.close()  ##  Closing the contact with the database
+add_data(dfob)   
+con.close()  
